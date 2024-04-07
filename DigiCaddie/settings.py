e5f64e3bc7887b83a7e38e9a0a6605ddfae5b98e
Fletcherspielman <fletcher.spiel@gmail.com>
2024-04-06 17:14:27 -0700

    tuple error

--- DigiCaddie/settings.py
+++ DigiCaddie/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MEDIA_URL = 'mediafiles/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
