386af65871202df4610045c53326b7235f3d09e1
Fletcherspielman <fletcher.spiel@gmail.com>
2024-04-07 13:51:28 -0700

    url error

--- DigiCaddie/settings.py
+++ DigiCaddie/settings.py
@@ -155,13 +155,13 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'cs333final'
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f'cs333final.s3.us-east-1.amazonaws.com/'
+    AWS_S3_CUSTOM_DOMAIN = f'cs333final.s3.amazonaws.com/'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = 'https://cs333final.s3.us-east-1.amazonaws.com/media'
+    MEDIA_URL = 'https://cs333final.s3.amazonaws.com/media/'
     DEFAULT_FILE_STORAGE = 'DigiCaddie.storage_backends.PublicMediaStorage'
     AWS_LOCATION = 'static'
-    STATIC_URL = 'https://cs333final.s3.us-east-1.amazonaws.com/static'
+    STATIC_URL = 'https://cs333final.s3.amazonaws.com/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
